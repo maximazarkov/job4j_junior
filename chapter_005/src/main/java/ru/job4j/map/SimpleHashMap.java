ad26d6336cfe134b8dd171af4f5dc366bd386e64
Maxim Azarkov <aza-maxim@yandex.ru>
2020-04-14 23:58:43 +0700

    Реализовать собственную структуру данных - HashMap[#204063]+refactor

--- chapter_005/src/main/java/ru/job4j/map/SimpleHashMap.java
+++ chapter_005/src/main/java/ru/job4j/map/SimpleHashMap.java
@@ -4,14 +4,14 @@ public class  SimpleHashMap<K, V> {
     private final static int DEFAULT_ARRAY_SIZE = 1 << 4; // aka 16
     private final static float DEFAULT_LOAD_FACTOR = 0.75f;
 
-    private Node<K, V>[] sHashMapArray;
+    private Node<K, V>[] table;
     private int size;
     private int count = 0;
     private float loadFactor = 0;
 
     public SimpleHashMap() {
          size = DEFAULT_ARRAY_SIZE;
-         sHashMapArray = new Node[size];
+         table = new Node[size];
     }
 
     static class Node<K, V> {
@@ -26,45 +26,47 @@ public class  SimpleHashMap<K, V> {
 
     public boolean insert(K key, V value) {
         boolean result = false;
+        increase(hash(key));
         int h = hash(key);
-        if (loadFactor > DEFAULT_LOAD_FACTOR) {
-            Node<K, V>[] tmp = sHashMapArray;
-            size = size << 1;
-            sHashMapArray = new Node[size];
-            int index = 0;
-            while (index < tmp.length) {
-                if (tmp[index] != null) {
-                    h = hash(tmp[index].key);
-                    sHashMapArray[h] = new Node<>(tmp[index].key, tmp[index].value);
-                }
-                index++;
-            }
-        }
-        h = hash(key);
-        if (sHashMapArray[h] == null) {
-            sHashMapArray[h] = new Node<>(key, value);
+        if (table[h] == null) {
+            table[h] = new Node<>(key, value);
             loadFactor = (float) ++count / size;
             result = true;
         }
         return result;
     }
 
+    private void increase(int h) {
+        if (loadFactor > DEFAULT_LOAD_FACTOR) {
+            Node<K, V>[] tmp = table;
+            size = size << 1;
+            table = new Node[size];
+            int index = 0;
+            while (index < tmp.length) {
+                if (tmp[index] != null) {
+                    h = hash(tmp[index].key);
+                    table[h] = new Node<>(tmp[index].key, tmp[index].value);
+                }
+                index++;
+            }
+        }
+    }
+
+
     public int hash(K key) {
-//        int result = 1;
-//        result = (31 * result + key.hashCode()) % size;
         return key.hashCode() % size;
     }
 
     public V get(K key) {
         int h = hash(key);
-        return (sHashMapArray[h] != null && key.equals(sHashMapArray[h].key)) ? sHashMapArray[h].value : null;
+        return (table[h] != null && key.equals(table[h].key)) ? table[h].value : null;
     }
 
     public boolean delete(K key) {
         int h = hash(key);
         boolean result = false;
-        if (sHashMapArray[h] != null && key.equals(sHashMapArray[h].key)) {
-            sHashMapArray[h] = null;
+        if (table[h] != null && key.equals(table[h].key)) {
+            table[h] = null;
             count--;
             result = true;
         }
