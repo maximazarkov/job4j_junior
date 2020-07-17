de91ead48738bec5b1c8cbf78ad447872d3b53bb
Maxim Azarkov <aza-maxim@yandex.ru>
2020-07-17 09:54:15 +0700

    Задача к контрольным вопросам [#204045]+refactoring

--- chapter_005/src/main/java/ru/job4j/article/Article.java
+++ chapter_005/src/main/java/ru/job4j/article/Article.java
@@ -1,37 +1,18 @@
 package ru.job4j.article;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.lang.reflect.Array;
+import java.util.*;
 
 public class Article {
     public static boolean generateBy(String origin, String line) {
         String[] words = origin.split("[\\s,.!]");
-        Map<String, Integer> orgnFreq = new HashMap<>();
-        for (String w : words) {
-            Integer n = orgnFreq.get(w);
-            n = (n == null) ? 1 : ++n;
-            orgnFreq.put(w, n);
-        }
+        HashSet<String> orgnWords = new HashSet<>(Arrays.asList(origin.split("[\\s.,!]")));
+        HashSet<String> lineWords = new HashSet<>(Arrays.asList(line.split("[\\s.,!]")));
 
-        words = line.split("[\\s,.!]");
-        Map<String, Integer> lineFreq = new HashMap<>();
-        for (String w : words) {
-            Integer n = lineFreq.get(w);
-            n = (n == null) ? 1 : ++n;
-            lineFreq.put(w, n);
-        }
+        Iterator<String> it = lineWords.iterator();
 
-        for (String w : words) {
-            // в этом цикле можно декрементировать позаимствованные слова из первой мапы,
-            //но я этого делать не стал. просто ловлю совпадения
-            Integer o = orgnFreq.get(w);
-            if (o == null) {
-                return false;
-            }
-
-            Integer l = lineFreq.get(w);
-
-            if (l > o) {
+        while (it.hasNext()) {
+            if (!orgnWords.contains(it.next())) {
                 return false;
             }
         }
