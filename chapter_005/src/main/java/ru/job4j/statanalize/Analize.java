52d9e19328e8edeef3575a88cdb4cc430a7a1c48
Maxim Azarkov <aza-maxim@yandex.ru>
2020-07-21 14:06:54 +0700

    Статистика по коллекции. [#204044]+refactoring на одной мапе

--- chapter_005/src/main/java/ru/job4j/statanalize/Analize.java
+++ chapter_005/src/main/java/ru/job4j/statanalize/Analize.java
@@ -1,10 +1,6 @@
 package ru.job4j.statanalize;
 
-import java.sql.PreparedStatement;
 import java.util.*;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Analize {
     /**
@@ -24,85 +20,24 @@ public class Analize {
             return info;
         }
 
-        Map<Integer, String> previousMap =
-                previous.stream().collect(Collectors.toMap(User::getId,  User::getName));
-        Iterator<Map.Entry<Integer, String>> preIterator = previousMap.entrySet().iterator();
-        HashMap.Entry<Integer, String> preEntry = null;
+        Map<Integer, String> map = new HashMap<>();
+        for (User user : previous) {
+            map.put(user.id, user.name);
+        }
 
-        Map<Integer, String> currentMap =
-                current.stream().collect(Collectors.toMap(User::getId,  User::getName));
-        Iterator<Map.Entry<Integer, String>> curIterator = currentMap.entrySet().iterator();
-        HashMap.Entry<Integer, String> curEntry = null;
-
-        do {
-            System.out.println(info.added + " " + info.deleted + " " + info.changed);
-
-            preEntry = setEntry(preEntry, preIterator);
-            curEntry = setEntry(curEntry, curIterator);
-
-            int tail = checkingTail(preEntry, curEntry, curIterator);
-            if (tail > 0) {
-                info.added += tail;
-                break;
-            }
-
-            tail = checkingTail(curEntry, preEntry, preIterator);
-            if (tail > 0) {
-                info.deleted += tail;
-                break;
-            }
-
-            if (preEntry.getKey() - curEntry.getKey() > 0) {
-                info.added++;
-                curEntry = null;
-                continue;
-            }
-
-            if (preEntry.getKey() - curEntry.getKey() < 0) {
-                info.deleted++;
-                preEntry = null;
-                continue;
-            }
-
-            if (preEntry.getKey() - (curEntry.getKey()) == 0) {
-                if (!preEntry.getValue().equals(curEntry.getValue())) {
+        for (User user : current) {
+            boolean checkKey = map.containsKey(user.id);
+            if (checkKey) {
+                if (!user.name.equals(map.get(user.id))) {
                     info.changed++;
                 }
-                curEntry = null;
-                preEntry = null;
+            } else {
+                info.added++;
             }
-
-        } while ((preIterator.hasNext())
-                || (curIterator.hasNext())
-                || curEntry != null
-                || preEntry != null);
+        }
+        info.deleted = previous.size() + info.added - current.size();
 
         return info;
-    }
-
-    private int checkingTail(HashMap.Entry<Integer, String> entryNull,
-                             HashMap.Entry<Integer, String> entryTail,
-                             Iterator<Map.Entry<Integer, String>> it) {
-        int count = 0;
-        HashMap.Entry<Integer, String> tail = entryTail;
-        if (entryNull == null) {
-            do {
-                count++;
-                tail = it.hasNext() ? it.next() : null;
-            } while (tail != null);
-        }
-        return count;
-    }
-
-    private HashMap.Entry<Integer, String> setEntry(HashMap.Entry<Integer, String> entry,
-                                                    Iterator<Map.Entry<Integer, String>> it) {
-        HashMap.Entry<Integer, String> result = entry;
-        if (entry == null) {
-            if (it.hasNext()) {
-                result = it.next();
-            }
-        }
-        return result;
     }
 
     public static class User {
@@ -112,31 +47,6 @@ public class Analize {
         User(int id, String name) {
             this.id = id;
             this.name = name;
-        }
-
-        private int getId() {
-            return id;
-        }
-
-        private String getName() {
-            return name;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) {
-                return true;
-            }
-            if (o == null || getClass() != o.getClass()) {
-                return false;
-            }
-            User user = (User) o;
-            return id == user.id && Objects.equals(name, user.name);
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(id, name);
         }
     }
 
