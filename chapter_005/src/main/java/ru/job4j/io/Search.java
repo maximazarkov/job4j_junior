7f739853992962cefd8fe6c58482dc6972f2f98b
Maxim Azarkov <aza-maxim@yandex.ru>
2020-08-24 01:34:57 +0700

    3. Сканирование файловой системы. [#204084] Save Intermediate Result

--- chapter_005/src/main/java/ru/job4j/io/Search.java
+++ chapter_005/src/main/java/ru/job4j/io/Search.java
@@ -4,10 +4,22 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 
 public class Search {
     public static void main(String[] args) throws IOException {
         Path start = Paths.get(".");
-        Files.walkFileTree(start, new PrintFiles());
+        search(start, "js").forEach(System.out::println);
+    }
+
+
+    public static List<Path> search(Path root, String ext) throws IOException {
+        SearchFiles searcher = new SearchFiles(p -> {
+            return p.toFile()
+                    .getName
+                    .endsWith(ext);
+        });
+        Files.walkFileTree(root, searcher);
+        return searcher.getPaths();
     }
 }
