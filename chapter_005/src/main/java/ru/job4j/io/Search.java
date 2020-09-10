a5f5e97998a30ec49cad0f555d46508d357f76e5
Maxim Azarkov <aza-maxim@yandex.ru>
2020-09-11 00:47:35 +0700

    3. ñêàíèðîâàíèå ôàéëîâîé ñèñòåìû [#204084].please help

--- chapter_005/src/main/java/ru/job4j/io/Search.java
+++ chapter_005/src/main/java/ru/job4j/io/Search.java
@@ -3,14 +3,28 @@ package ru.job4j.io;
 import java.io.File;
 import java.io.FilenameFilter;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class Search {
+
+    public static FilenameFilter searchFilter(final  String regex) {
+        //создадим анонимный внутренний класс
+        return new FilenameFilter() {
+            private Pattern pattern = Pattern.compile(regex);
+            @Override
+            public boolean accept(File dir, String name) {
+                return pattern.matcher(name).matches();
+            }
+        }; // конец анонимного класса
+    }
+
     public static void main(String[] args) throws IOException {
         String dirname = "c:\\projects\\job4j_junior";  //.
         String ext = "txt";
@@ -22,9 +36,52 @@ public class Search {
 
         //второй костыльный вариант, с применением listFiles()
         searchLF(start, ext).forEach(System.out::println);
-//        search(start, ext).forEach(System.out::println);
+
+        // метод по заданию
+        search(start, ext).forEach(System.out::println);
+
+        // альтернативный метод через Pattern
+        searchPattern(start, ext).forEach(System.out::println);
     }
 
+    // т.к. пока не могу понять
+    public static List<Path> search(Path root, String ext) throws IOException {
+        SearchFiles searcher = new SearchFiles(p -> p
+                .toFile()
+                .getName()
+                .endsWith(ext)
+        );
+
+
+        Files.walkFileTree(root, searcher);
+        return searcher.getPaths();
+    }
+
+    public static List<Path> searchPattern(Path root, String ext) throws IOException {
+//        SearchFiles searcher = new SearchFiles(p -> {
+//            Arrays.asList(p.toFile().list(
+//                    new FilenameFilter() {
+//                        private Pattern pattern = Pattern.compile(ext);
+//                        @Override
+//                        public boolean accept(File dir, String name) {
+//                            return pattern.matcher(name).matches();
+//                        }
+//                    })
+//            );
+//            return false;   // не понятно, почему здесь требуется return false.
+//        });
+
+//        Files.walkFileTree(root, searcher);
+//        return searcher.getPaths();
+        return null;
+    }
+
+    /**
+     * метод возвращает коллекцию найденных файлов с помощью метода listFiles() и интерфейса FileFilter
+     * @param dirname - рабоачая директория, где производится поиск
+     * @param ext - расширение
+     * @return - возвращает список найденных файлов в виде коллекции
+     */
     public static List<Path> searchLF(Path dirname, String ext) {
         File f = dirname.toFile();
         FilenameFilter ff = new OnlyExt(ext);
@@ -33,6 +90,12 @@ public class Search {
                 .collect(Collectors.toList());
     }
 
+    /**
+     *  метод возвращает коллекцию найденных файлов с помощью интерфейса FileFilter
+     * @param dirname - рабоачая директория, где производится поиск
+     * @param ext - расширение
+     * @return - возвращает список найденных файлов в виде коллекции
+     */
     public static List<Path> searchFF(Path dirname, String ext) {
         File f = dirname.toFile();
         FilenameFilter ff = new OnlyExt(ext);
@@ -40,19 +103,12 @@ public class Search {
                 .map(p -> Paths.get(p))
                 .collect(Collectors.toList());
     }
-// т.к. пока не могу понять
-//    public static List<Path> search(Path root, String ext) throws IOException {
-//        SearchFiles searcher = new SearchFiles(p -> {
-//            return p.toFile()
-//                    .getName
-//                    .endsWith(ext);
-//        });
-//        Files.walkFileTree(root, searcher);
-//        return searcher.getPaths();
-//    }
 }
 
-class OnlyExt implements FilenameFilter{
+/**
+ * Реализцем интерфейс FilenameFilter
+ */
+class OnlyExt implements FilenameFilter {
     String ext;
     public OnlyExt(String ext) {
         this.ext = "." + ext;
