78b16a258fe8549f3f2c2686322d82f14987e6ff
Maxim Azarkov <aza-maxim@yandex.ru>
2020-09-23 01:13:23 +0700

    4. Àðõèâèðîâàòü ïðåêòþ 4.[#204087].ERROR

--- chapter_005/src/main/java/ru/job4j/io/Zip.java
+++ chapter_005/src/main/java/ru/job4j/io/Zip.java
@@ -1,13 +1,20 @@
 package ru.job4j.io;
 
 import java.io.*;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
+
+import static ru.job4j.io.Search.search;
 
 public class Zip {
 
@@ -21,6 +28,11 @@ public class Zip {
                 source = it.next();
                 zip.putNextEntry(new ZipEntry(source.getPath()));
                 try (BufferedInputStream out = new BufferedInputStream(
+                        // на этом месте при архивации одного файла или директории, ошибки не происходит
+                        // если файлов архивируется много, то программа падает с ошибкой
+                        // java.io.FileNotFoundException: c:\projects\job4j_junior (Отказано в доступе)
+                        // при этом запись c:\projects\job4j_junior выглядит как файл без расширенияи на
+                        // директорию не похож. видимо в этом проблема
                         new FileInputStream(source.getPath()))) {
                     zip.write(out.readAllBytes());
                 }
@@ -34,29 +46,32 @@ public class Zip {
         List<File> sources = new LinkedList<>();
         sources.add(source);
         packFiles(sources, target);
-//        try (ZipOutputStream zip = new ZipOutputStream(
-//                new BufferedOutputStream(
-//                        new FileOutputStream(target)))) {
-//            zip.putNextEntry(new ZipEntry(source.getPath()));
-//            try (BufferedInputStream out = new BufferedInputStream(
-//                    new FileInputStream(source))) {
-//
-//                zip.write(out.readAllBytes());
-//            }
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
     }
 
-    public static void main(String[] args) {
+    public static List<File> convertListPathToFile(List<Path> in) {
+        Iterator<Path> it = in.iterator();
+        List<File> out = new LinkedList<>();
+        while (it.hasNext()) {
+            out.add(it.next().toFile());
+        }
+        return out;
+    }
+
+    public static void main(String[] args) throws IOException {
         new Zip().packSingleFile(
                 new File("./chapter_005/pom.xml"),
-//                new File("c:/projects/job4j_junior/chapter_005/pom.xml"),
                 new File("./chapter_005/pom.zip")
         );
         String dirname = "c:/projects/job4j_junior";
-        String ext = "";
+        File target =new File("./chapter_005/project.zip");
+        String ext = "class";
         Path start = Paths.get(dirname);
+        List<Path> sourcePaths = search(start, p -> !(p
+                .toFile()
+                .getAbsolutePath()
+                .endsWith("." + ext)));
 
+        System.out.println();
+        new Zip().packFiles(convertListPathToFile(sourcePaths), target);
     }
 }
