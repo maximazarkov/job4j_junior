a787ca070dee947ce171aa26f6831d2a2cbc09ce
Maxim Azarkov <aza-maxim@yandex.ru>
2020-10-26 01:56:43 +0700

    1. EchoServerBot_204097_init

--- chapter_005/src/main/java/ru/job4j/io/EchoServer.java
+++ chapter_005/src/main/java/ru/job4j/io/EchoServer.java
@@ -3,11 +3,32 @@ package ru.job4j.io;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.regex.Pattern;
 
 public class EchoServer {
 
+    private static final String EXIT = "Good bye!";
+    private static final String HELLO = "Hello, dear friend.";
     private boolean notExit = true;
+    private String answer = "";
+    private static final Charset UTF_8 = StandardCharsets.UTF_8;
+    private static final Charset ASCII = StandardCharsets.US_ASCII;
+
+    private void checkMsg(String str) {
+        boolean b = str.contains("msg=Exit");
+        if (b) {
+            notExit = false;
+            answer = EXIT;
+        }
+        b = str.contains("msg=Hello");
+        if (b) {
+            answer = HELLO;
+        } else {
+            answer = str;
+        }
+    }
 
     private void run() throws IOException {
         try (ServerSocket server = new ServerSocket(9000)) {
@@ -18,13 +39,19 @@ public class EchoServer {
                              new InputStreamReader(socket.getInputStream()))) {
                     String str;
                     while (!(str = in.readLine()).isEmpty()) {
+
                         System.out.println(str);
-                        boolean b = Pattern.matches("Bye", str);
-                        if(!b) {
-                            notExit = false;
-                        }
+
+                        checkMsg(str);
+
                     }
-                    out.write("HTTP/1.1 200 OK\r\n\\".getBytes());
+                    out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                    out.write("==========".getBytes());
+                    out.write(System.lineSeparator().getBytes());
+                    out.write(answer.getBytes(UTF_8));
+//                    out.write(answer.getBytes(ASCII));
+                    out.write(System.lineSeparator().getBytes());
+                    out.write("==========".getBytes());
 
                 }
             }
