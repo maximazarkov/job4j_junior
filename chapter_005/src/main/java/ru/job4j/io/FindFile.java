35d9bbc4c5918f4d040f902e1fae5cbd908f2079
Maxim Azarkov <aza-maxim@yandex.ru>
2020-12-03 02:46:00 +0700

    FindFile #204093. add find on FULLMASK

--- chapter_005/src/main/java/ru/job4j/io/FindFile.java
+++ chapter_005/src/main/java/ru/job4j/io/FindFile.java
@@ -18,30 +18,38 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Objects;
+import java.util.regex.Pattern;
+
+import static ru.job4j.io.Search.search;
 
 public class FindFile {
     private static String sourceDir;
     private static String fileNameMask;
     private static String resultFile;
     private static String typeMask;
-
-    private static String pathLog;
-
     private static StringBuilder data = new StringBuilder();
 
+    private static final String MFULL = "full";
+    private static final String MMASK = "mask";
+    private static final String MREGULAR = "regular";
+    private static final String FORMATCOMMAND = "format: java -jar find.jar -d sourceDir -n mask -m|-f|-r -o resultFile"
+            + System.lineSeparator()
+            + "параметры:"
+            + System.lineSeparator()
+            + "-d          -ключ для указания директории (sourceDir), в которой будет производиться поиск"
+            + System.lineSeparator()
+            + "-m          -ключ для указания маски (mask), по которой будет производиться поиск"
+            + System.lineSeparator()
+            + "-m|-f|-r    -ключ для указания типа маски. искать по маске, либо -f - полное совпадение имени. -r регулярное выражение"
+            + System.lineSeparator()
+            + "-o          -ключ для указания имени log-файла (resultFile)";
     static void parseArgs(String[] args) {
-        String FORMATCOMMAND = "format: java -jar find.jar -d sourceDir -n mask -m|-f|-r -o resultFile"
-                + System.lineSeparator()
-                + "параметры:"
-                + System.lineSeparator()
-                + "-d          -ключ для указания директории (sourceDir), в которой будет производиться поиск"
-                + System.lineSeparator()
-                + "-m          -ключ для указания маски (mask), по которой будет производиться поиск"
-                + System.lineSeparator()
-                + "-m|-f|-r    -ключ для указания типа маски. искать по маске, либо -f - полное совпадение имени. -r регулярное выражение"
-                + System.lineSeparator()
-                + "-o          -ключ для указания имени log-файла (resultFile)";
+
         int countNormalKeys = 0;
         if (args.length >= 7) {
             int indexArgs = 0;
@@ -49,6 +57,10 @@ public class FindFile {
                 switch (args[indexArgs]) {
                     case "-d" :
                         sourceDir = args[++indexArgs];
+                        File file = new File(sourceDir);
+                        if(!file.exists()) {
+                            System.out.println(sourceDir + " папка не существует");
+                        }
                         countNormalKeys++;
                         break;
                     case "-n" :
@@ -56,15 +68,15 @@ public class FindFile {
                         countNormalKeys++;
                         break;
                     case "-m":
-                        typeMask = "mask";
+                        typeMask = MMASK;
                         countNormalKeys++;
                         break;
                     case "-f":
-                        typeMask = "full";
+                        typeMask = MFULL;
                         countNormalKeys++;
                         break;
                     case "-r":
-                        typeMask = "regular";
+                        typeMask = MREGULAR;
                         countNormalKeys++;
                         break;
                     case "-o":
@@ -82,7 +94,7 @@ public class FindFile {
             System.out.println("Вы ввели недостаточной число параметров. Формат комманды:");
             System.out.println(FORMATCOMMAND);
             if (countNormalKeys != 4) {
-
+                System.out.println(FORMATCOMMAND);
             }
         }
     }
@@ -108,24 +120,63 @@ public class FindFile {
         }
     }
 
-    private void getResultFindFile() {
+    private void getResultFindFile(List<Path> files) {
         appendData(data, "Место поиска:", sourceDir);
         appendData(data, "Тип маски:", typeMask);
         appendData(data, "Маска поиска:", fileNameMask);
         appendData(data, "-----------------");
         appendData(data, "Результат поиска: ");
+        for (Path path : files) {
+            appendData(data, path.toString());
+        }
         appendData(data, "-----------------");
-        appendData(data, "Всего файлов в директории: 0");
-        appendData(data, "Найде файлов: 0");
+        appendData(data, "Найдено файлов: " + files.size());
+    }
+
+    private List<Path> findFile() throws IOException {
+        LinkedList fileList = new LinkedList();
+        //работает!
+        if (typeMask.equals(MFULL)) {
+            search(Paths.get(sourceDir), p -> p
+                    .toFile()
+                    .getAbsolutePath()
+                    .endsWith(fileNameMask)
+            ).forEach(p -> fileList.add(p.toAbsolutePath()));
+        }
+        //todo можно рассмотреть два варианта решения. 1.парсить строку и и делать сложные цикцы сравнения. 2.попробовать преобразовать маску в регулярное выражение
+        if (typeMask.equals(MMASK)) {
+            search(Paths.get(sourceDir), p -> p
+                    .toFile()
+                    .getAbsolutePath()
+//                    .toLowerCase()
+                    .endsWith("*.md")
+            ).forEach(p -> fileList.add(p.toAbsolutePath()));
+        }
+        //todo решиь вопрос с поиском через регулярное выражение
+        if (typeMask.equals(MREGULAR)) {
+            Pattern regMask = Pattern.compile(fileNameMask);
+            search(Paths.get(sourceDir), p -> p
+                    .toFile()
+                    .getAbsolutePath()
+                    .endsWith(fileNameMask)
+//            ).forEach(p -> {
+//                fileList.add(regMask.matcher((CharSequence) p.toAbsolutePath().toString()));
+//            });
+            ).forEach(p -> {
+                regMask.matcher((CharSequence) p.toAbsolutePath().toString());
+            });
+        }
+        return fileList;
     }
 
     public static void main(String[] args) throws IOException {
         FindFile ff = new FindFile();
         ff.parseArgs(args);
-        
-        ff.getResultFindFile();
+
+        ff.getResultFindFile(ff.findFile());
 
         ff.writeFileLog();
 
     }
+
 }
