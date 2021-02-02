6e97dd1193c2d58c7673f662078ba6ae36e1477b
Maxim Azarkov <aza-maxim@yandex.ru>
2021-02-03 00:33:12 +0700

    filters #204031-bugFix

--- chapter_005/src/main/resources/204031_filters_select.sql
+++ chapter_005/src/main/resources/204031_filters_select.sql
@@ -28,7 +28,7 @@ SELECT name FROM product WHERE name ~ '(М|м)ороженное';
 --SELECT Extract(MONTH FROM current_date);
 SELECT name, expired_date
 FROM product
-WHERE Extract(MONTH FROM expired_date) = (Extract(MONTH FROM current_date) + 1);
+WHERE Extract(MONTH FROM expired_date) = (Extract(MONTH FROM current_date  + INTERVAL '1 month'));
 
 ----4. Написать запрос, который выводит самый дорогой продукт
 ---- product(id, name, type_id, expired_date, price)
@@ -50,26 +50,16 @@ SELECT p.name AS product_name, t.name AS type_name
 FROM product AS p
 JOIN type AS t
 ON p.type_id = t.id
-WHERE
-    p.type_id = (SELECT t.id AS type_id FROM type AS t WHERE t.name ~ '^((С|с)ыр)')
-    OR p.type_id = (SELECT t.id AS type_id FROM type AS t WHERE t.name ~ '^((М|м)олоко)');
-
+WHERE p.type_id
+    IN (SELECT t.id AS type_id FROM type AS t WHERE t.name ~ '^((С|с)ыр)' OR t.name ~ '^((М|м)олоко)');
 
 ---- 7. Написать запрос, который выводит тип продуктов, которых осталось меньше 10 штук.
----- сначала вытащим все типы из таблицы product меньше 10
---SELECT p.type_id AS count_prod FROM product AS p GROUP BY p.type_id HAVING COUNT(p.type_id) < 10;
-
----- затем выведем типы через JOIN объединив запросы
 SELECT t.name AS type_product
 FROM type AS t
 JOIN product AS p
 ON t.id = p.type_id
-WHERE p.type_id IN
-    (SELECT p.type_id AS count_prod
-    FROM product AS p
-    GROUP BY p.type_id
-    HAVING COUNT(p.type_id) < 10)
-GROUP BY t.name;
+GROUP BY t.name
+HAVING COUNT(p.name) < 10;
 
 ---- 8. Вывести все продукты и их тип.
 SELECT p.name, t.name
