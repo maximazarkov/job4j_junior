7630e216eda8c7dcdf8c9824101c6ce5248922c1
Maxim Azarkov <aza-maxim@yandex.ru>
2021-02-05 00:14:57 +0700

    storage #204034-bugFix

--- chapter_005/src/main/resources/204034_storage_select.sql
+++ chapter_005/src/main/resources/204034_storage_select.sql
@@ -1,23 +1,20 @@
 \c mstorage
 
 -- 1. Вывести список всех машин и все привязанные к ним детали.
-SELECT * FROM cars c
-LEFT JOIN bodycars b ON c.bc_id = b.id
-LEFT JOIN engines e ON c.en_id = e.id
-LEFT JOIN transmissions t ON c.tr_id = t.id;
-
--- тот же запрос, но немного компактнее вывод
 SELECT c.name car, b.name body, e.name engine, t.name transmissions FROM cars c
-LEFT JOIN bodycars b ON c.bc_id = b.id
-LEFT JOIN engines e ON c.en_id = e.id
-LEFT JOIN transmissions t ON c.tr_id = t.id;
+INNER JOIN bodycars b ON c.bc_id = b.id
+INNER JOIN engines e ON c.en_id = e.id
+INNER JOIN transmissions t ON c.tr_id = t.id;
 
 -- 2. Вывести отдельно детали, которые не используются в машине, кузова, двигатели, коробки передач.
--- ВНИМАНИЕ! Возможно этот запрос работает не верно из-за неверного понимания ТЗ.
--- TODO необходимо уточнить у ментора, исправить
-SELECT c.name car, b.name body, e.name engine, t.name transmissions FROM cars c
-FULL JOIN bodycars b ON c.bc_id = b.id
-FULL JOIN engines e ON c.en_id = e.id
-FULL JOIN transmissions t ON c.tr_id = t.id
-WHERE c.bc_id is null OR c.en_id is null OR c.tr_id is null
-GROUP BY c.name, b.name, e.name, t.name;
+SELECT b.name body FROM bodycars b
+LEFT JOIN cars c ON c.bc_id = b.id
+WHERE c.bc_id is null;
+
+SELECT e.name engine FROM engines e
+LEFT JOIN cars c ON c.en_id = e.id
+WHERE c.en_id is null;
+
+SELECT t.name transmissions FROM transmissions t
+LEFT JOIN cars c ON c.tr_id = t.id
+WHERE c.tr_id is null;
