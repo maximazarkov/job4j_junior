c8ce0c3040d1f999d4549ac19b7e97a8f9be8636
Maxim Azarkov <aza-maxim@yandex.ru>
2023-04-02 10:21:34 +0700

    3. Автоупаковка / автораспаковка [#504971]

---
+++ src/main/java/ru/job4j/autopacking/Box.java
@@ -0,0 +1,43 @@
+package ru.job4j.autopacking;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Box {
+    public static void num(Integer x) {
+        System.out.println("Значение типа Integer: " + x);
+    }
+
+    public static void num(int x) {
+        System.out.println("Значение типа int: " + x);
+    }
+
+    public static void main(String[] args) {
+        int a = 1;
+        Integer b;
+        b = a;
+        System.out.println("Integer b = " + b);
+        num(5);
+        Integer m = 3;
+        int n = m;
+        System.out.println("int n = " + n);
+        Integer k = 5;
+        Integer l = 6;
+        System.out.println(k * l);
+        Integer g = 250;
+        Integer h = 250;
+        System.out.println(g == h);
+        Integer v = 127;
+        Integer w = 127;
+        System.out.println(v == w);
+        Integer sum = 0;
+        for (int i = 128; i < 300; i++) {
+            sum += i;
+        }
+        System.out.println(sum);
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            list.add(i);
+        }
+    }
+}
